6b0f9c222166778c6dbf5d036e046f4584760edf
Javier Escudero <javierparkour@gmail.com>
2019-12-27 15:24:47 -0500

    initial commit

---
+++ Image_Enhacements/CurrentExtent.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Linq;
+using ArcGIS.Desktop.Framework.Contracts;
+using ArcGIS.Desktop.Mapping;
+using ArcGIS.Desktop.Framework.Threading.Tasks;
+using ArcGIS.Core.CIM;
+using ArcGIS.Desktop.Core.Geoprocessing;
+using ArcGIS.Core.Data;
+using ArcGIS.Core.Data.Raster;
+using System.IO;
+using System.Collections.Generic;
+using MessageBox = ArcGIS.Desktop.Framework.Dialogs.MessageBox;
+
+
+namespace Image_Enhacements
+{
+    internal class CurrentExtentStatistics : Button
+    {
+
+        private List<Double> CalculateBandStatistics(Raster raster, int bandIndex)
+        {
+            RasterCursor rasterCursor = raster.CreateCursor(500, 500);
+            List<Double> blockValues = new List<Double>();
+            do
+            {
+                PixelBlock currentPixelBlock = rasterCursor.Current;
+                Array sourcePixeles = currentPixelBlock.GetPixelData(bandIndex, true);
+                int pixelBlockHeight = currentPixelBlock.GetHeight();
+                int pixelBlockWidth = currentPixelBlock.GetWidth();
+                foreach (var valuePixel in sourcePixeles)
+                    blockValues.Add(Convert.ToDouble(valuePixel));
+            }
+            while (rasterCursor.MoveNext());
+
+            var typicalBlockValues = Statistical.ExcludeOuliers(blockValues, 0.0025);
+            var statistics = Statistical.CalculateStatistics(typicalBlockValues);
+
+            return statistics;
+        }
+
+        protected async override void OnClick()
+        {
+
+            // Map and scale validations
+            var mapView = MapView.Active;
+            if (mapView != null)
+            {
+                var scale = mapView.Camera.Scale;
+                if (scale > 35000)
+                {
+                    MessageBox.Show(string.Format("Map extent is too big (Map Scale: {0}). Zoom in to scales under 35.000.", (int)scale));
+                    return;
+                }
+            }
+            else
+            {
+                return;
+            }
+
+            // Get selected layer
+            var selectedLayer = mapView.GetSelectedLayers().OfType<BasicRasterLayer>().FirstOrDefault();
+            if (selectedLayer != null)
+            {
+                // Clipping image 
+                var progressDialog = new ProgressDialog("Getting statistics", "Cancel", 100, true);
+                progressDialog.Show();
+                var progressSource = new CancelableProgressorSource(progressDialog);
+
+                var clippingResult = await QueuedTask.Run(() =>
+                {
+                    string tool_path = "management.clip";
+                    string output = @"in_memory\raster";
+                    var rectangle = string.Format("{0} {1} {2} {3}", mapView.Extent.XMin, mapView.Extent.YMin, mapView.Extent.XMax, mapView.Extent.YMax);
+
+                    var args = Geoprocessing.MakeValueArray((RasterLayer)selectedLayer, "", output);
+                    var envs = Geoprocessing.MakeEnvironmentArray(extent: rectangle);
+                    var flags = GPExecuteToolFlags.None; // GPExecuteToolFlags.AddOutputsToMap;
+                    var result = Geoprocessing.ExecuteToolAsync(tool_path, args, envs, progressSource.Progressor, flags);
+
+                    return result;
+                });
+                progressDialog.Hide();
+
+                // Getting statistics fro current extent
+                await QueuedTask.Run(() =>
+                {
+                    var clippedRaster = clippingResult.Values[0];
+                    string baseName = Path.GetFileName(clippedRaster);
+                    string folderName = Path.GetDirectoryName(clippedRaster);
+                    var geodatabasePath = new FileGeodatabaseConnectionPath(new Uri(folderName));
+                    var geodatabase = new Geodatabase(geodatabasePath);
+                    var rasterDataset = geodatabase.OpenDataset<RasterDataset>(baseName);
+                    Raster raster = rasterDataset.CreateFullRaster();
+                    CIMRasterRGBColorizer colorizer = (CIMRasterRGBColorizer)selectedLayer.GetColorizer();
+
+                    // Computing statistics 
+                    var redStatistics = CalculateBandStatistics(raster, colorizer.RedBandIndex);
+                    var greenStatistics = CalculateBandStatistics(raster, colorizer.GreenBandIndex);
+                    var blueStatistics = CalculateBandStatistics(raster, colorizer.BlueBandIndex);
+
+                    if (colorizer != null)
+                    {
+                        colorizer.StretchStatsType = RasterStretchStatsType.GlobalStats;
+                        colorizer.StretchType = RasterStretchType.Custom;
+
+                        // Red Band statistics
+                        colorizer.StretchStatsRed.mean = redStatistics[0];
+                        colorizer.StretchStatsRed.min = redStatistics[1];
+                        colorizer.StretchStatsRed.max = redStatistics[2];
+                        colorizer.StretchStatsRed.stddev = redStatistics[3];
+
+                        // Green Band statistics
+                        colorizer.StretchStatsGreen.mean = greenStatistics[0];
+                        colorizer.StretchStatsGreen.min = greenStatistics[1];
+                        colorizer.StretchStatsGreen.max = greenStatistics[2];
+                        colorizer.StretchStatsGreen.stddev = greenStatistics[3];
+
+                        // Bue band statistics
+                        colorizer.StretchStatsBlue.mean = blueStatistics[0];
+                        colorizer.StretchStatsBlue.min = blueStatistics[1];
+                        colorizer.StretchStatsBlue.max = blueStatistics[2];
+                        colorizer.StretchStatsBlue.stddev = blueStatistics[3];
+
+                        selectedLayer.SetColorizer(colorizer);
+                    }
+                });
+            }
+            else
+            {
+                MessageBox.Show("There is no Raster layer selected in TOC.");
+                return;
+            }
+        }
+    }
+}
---
+++ Image_Enhacements/Statistical.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Image_Enhacements
+{
+    class Statistical
+    {
+
+        public static List<Double> CalculateStatistics(List<double> values)
+        {
+            double stddev = 0;
+            double max = values.Max();
+            double min = values.Min();
+            double mean = values.Average();
+            double sum = values.Sum();
+            double n = values.Count();
+
+            if (n > 1)
+            {
+                double squares = values.Sum(value => (value - mean) * (value - mean));
+                stddev = Math.Sqrt(squares / n);
+            }
+            var statistics = new List<double>() { mean, min, max, stddev };
+
+            return statistics;
+        }
+
+        public static List<Double> ExcludeOuliers(List<double> values, double percentile)
+        {
+            // Absolute frecuencies
+            var absolute_frecuencies = new Dictionary<double, int>();
+            foreach (var key in values)
+            {
+                if (absolute_frecuencies.ContainsKey(key))
+                    absolute_frecuencies[key] += 1;
+                else
+                    absolute_frecuencies.Add(key, 1);
+            }
+
+            // Relative frecuencies
+            var relative_frecuencies = new Dictionary<double, double>();
+            double total = values.Count();
+            foreach (KeyValuePair<double, int> item in absolute_frecuencies)
+            {
+                var frecuency = item.Value / total;
+                relative_frecuencies[item.Key] = frecuency;
+            }
+
+            // Cumulative frecuencies
+            var probabilities = new Dictionary<double, double>();
+            var value_list = relative_frecuencies.Keys.ToList();
+            value_list.Sort();
+            double cumulativeFrecuency = 0;
+            foreach (var value in value_list)
+            {
+                var frecuency = relative_frecuencies[value];
+                cumulativeFrecuency += frecuency;
+                probabilities[value] = cumulativeFrecuency;
+            }
+
+            // Exclude values using percentile
+            var output_list = value_list.ToList();
+            foreach (var value in value_list)
+            {
+                var probability = probabilities[value];
+                if ((probability < percentile) | (probability > (1 - percentile)))
+                {
+                    output_list.Remove(value);
+                }
+            }
+            return output_list;
+        }
+    }
+}
